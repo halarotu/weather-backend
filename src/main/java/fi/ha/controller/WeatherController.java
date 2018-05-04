0adc8478514a5d3c620f57770183cbda42676ffe
halarotu <hannes.alarotu@gmail.com>
2018-05-04 23:59:46 +0300

    cors

--- src/main/java/fi/ha/controller/WeatherController.java
+++ src/main/java/fi/ha/controller/WeatherController.java
@@ -3,15 +3,13 @@ package fi.ha.controller;
 import fi.ha.domain.Observation;
 import fi.ha.repository.ObservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@CrossOrigin(origins = {"${allowed.origins}"})
 @Controller
 public class WeatherController {
 
